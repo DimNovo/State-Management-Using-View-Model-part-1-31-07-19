2ff8783b792149de870d9956cada1887631892ac
Dmitry Novosyolov <i1710@icloud.com>
2019-07-31 06:03:59 +0300

    Initial Commit

---
+++ State Management Using View Model/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  State Management Using View Model
+//
+//  Created by Dmitry Novosyolov on 31/07/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
